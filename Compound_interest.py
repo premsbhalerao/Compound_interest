a0881b76068d0b472e7f9005bbb7d212fa3b8e25
Prem Bhalerao <114722173+premsbhalerao@users.noreply.github.com>
2023-12-31 21:46:35 +0530

    File Adding

---
+++ Compound_interest.py
@@ -0,0 +1,6 @@
+def compound_interest(principal, rate, time):
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount - principal
+	print("Compound interest is", CI)
+
+compound_interest(10000, 10.25, 5)
